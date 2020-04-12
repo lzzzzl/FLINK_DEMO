9dd3511df8774427d756a8c9a1a53aec3f243caa
lzzzzl <597023590@qq.com>
2020-04-12 21:38:01 +0800

    kafka

--- src/main/java/myflink/RedisSinkTest.java
+++ src/main/java/myflink/RedisSinkTest.java
@@ -14,6 +14,7 @@ import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
 import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
 import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
 
+
 import java.util.Properties;
 import java.util.stream.Collector;
 
