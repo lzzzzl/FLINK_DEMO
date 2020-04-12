46c3a39366cf15ca59869256786ba0a041b06cee
lzzzzl <597023590@qq.com>
2020-04-12 21:30:38 +0800

    kafka

---
+++ src/main/java/myflink/RedisSinkExample.java
@@ -0,0 +1,28 @@
+package myflink;
+
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
+
+/**
+ * Created by 59702 on 2020/4/12.
+ */
+public class RedisSinkExample implements RedisMapper<Tuple2<String,Integer>> {
+
+
+    @Override
+    public RedisCommandDescription getCommandDescription() {
+        return new RedisCommandDescription(RedisCommand.SET, null);
+    }
+
+    @Override
+    public String getKeyFromData(Tuple2<String, Integer> data) {
+        return data.f0;
+    }
+
+    @Override
+    public String getValueFromData(Tuple2<String, Integer> data) {
+        return data.f1.toString();
+    }
+}
